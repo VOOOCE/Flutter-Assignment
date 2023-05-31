4d2e5934169e6178f53a4072f463d0f5b3d17d42
VOOOCE <hhammod736@gmail.com>
2023-05-31 23:36:01 +0300

    ThCommit

---
+++ lib/data/data_source.dart
@@ -0,0 +1,10 @@
+import 'package:todo_iug/models/task.dart';
+
+class DataSource {
+  static List<Task> tasks = [
+    Task("task 1",true),
+    Task("task 2",),
+    Task("task 3",true),
+    Task("task 4",true),
+   ];
+}
